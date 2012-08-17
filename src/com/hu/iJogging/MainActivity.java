21041991f714aac9194d5b907d1d38e8823fc0c0
chenpeng_hu <bupthcp@163.com>
2012-08-17 16:40:33 +0800

    增加onResume方法

--- src/com/hu/iJogging/MainActivity.java
+++ src/com/hu/iJogging/MainActivity.java
@@ -3,14 +3,36 @@ package com.hu.iJogging;
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 
+import android.os.Bundle;
+
 public class MainActivity extends SherlockFragmentActivity{
   private ActionBar mActionBar;
   private ActionBarAdapter mAdapter = null;
+
   
-  private void setActionBar(){
+  private void setupActionBar(){
     this.mActionBar = getSupportActionBar();
     this.mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
     this.mActionBar.setDisplayShowTitleEnabled(false);
     this.mActionBar.setDisplayShowHomeEnabled(false);
+  }
+
+  @Override
+  protected void onCreate(Bundle arg0) {
+    // TODO Auto-generated method stub
+    super.onCreate(arg0);
+  }
+
+  @Override
+  protected void onResume() {
+    // TODO Auto-generated method stub
+    super.onResume();
+    setupActionBar();
+  }
+
+  @Override
+  protected void onStart() {
+    // TODO Auto-generated method stub
+    super.onStart();
   }
 }
