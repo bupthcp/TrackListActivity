df11a98bc05ef7c4f1f6ea588b7b0a97574e4440
chenpeng_hu <bupthcp@163.com>
2012-08-17 16:34:41 +0800

    增加设置actionBar的方法，将actionbar设置为列表模式

--- src/com/hu/iJogging/MainActivity.java
+++ src/com/hu/iJogging/MainActivity.java
@@ -1,7 +1,16 @@
 package com.hu.iJogging;
 
+import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 
 public class MainActivity extends SherlockFragmentActivity{
-
+  private ActionBar mActionBar;
+  private ActionBarAdapter mAdapter = null;
+  
+  private void setActionBar(){
+    this.mActionBar = getSupportActionBar();
+    this.mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+    this.mActionBar.setDisplayShowTitleEnabled(false);
+    this.mActionBar.setDisplayShowHomeEnabled(false);
+  }
 }
