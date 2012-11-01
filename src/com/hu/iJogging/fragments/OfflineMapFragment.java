667a5bafa3dc2ef1970b1966948fdad71362a7fa
chenpeng_hu <bupthcp@163.com>
2012-11-01 17:48:47 +0800

    修改离线地图下载界面

--- src/com/hu/iJogging/fragments/OfflineMapFragment.java
+++ src/com/hu/iJogging/fragments/OfflineMapFragment.java
@@ -1,13 +1,16 @@
 package com.hu.iJogging.fragments;
 
 import com.baidu.mapapi.MKOLSearchRecord;
+import com.baidu.mapapi.MKOLUpdateElement;
 import com.baidu.mapapi.MKOfflineMap;
+import com.baidu.mapapi.Mj;
 import com.google.android.maps.mytracks.R;
 import com.hu.iJogging.IJoggingActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,14 +21,18 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
 
 public class OfflineMapFragment extends Fragment{
+  
+  private static String TAG =  "OfflineMapFragment";
   
   private IJoggingActivity mActivity;
   private MKOfflineMap mOffline = null;
   private View mFragmentView;
   private ListView listView;
-  private ArrayList<MKOLSearchRecord> installedMapList;  
+  private ArrayList<MKOLUpdateElement> installedMapList;  
   private ArrayList<MKOLSearchRecord> searchedMapList;
   
   @Override
@@ -45,21 +52,32 @@ public class OfflineMapFragment extends Fragment{
     EditText edittext = (EditText)mFragmentView.findViewById(R.id.search_bar_hot_city);
     edittext.setCompoundDrawablesWithIntrinsicBounds(R.drawable.search_bar_icon_normal, 0, 0, 0);
     listView = (ListView) mFragmentView.findViewById(R.id.list_installed);
-    installedMapList = mOffline.getOfflineCityList();
+    installedMapList = mOffline.getAllUpdateInfo();
+    Bundle a = new Bundle();
+    a.putInt("act", 16011500);
+    if (Mj.sendBundle(a) != 0){
+      Set<String> keySet = a.keySet();
+      Iterator<String> ii = keySet.iterator();
+      while(ii.hasNext()){
+        String str = (String) ii.next();
+        Log.i(TAG, "iterator"+str);
+      }
+    }
     View catalogView = View.inflate(mActivity,R.layout.offline_list_catalog, null);
     TextView catalogText =(TextView) catalogView.findViewById(R.id.list_item_catalog);
     catalogText.setText(R.string.strInstalledMap);
     listView.addHeaderView(catalogView);
     OfflineMapAdapter installedAdapter = new OfflineMapAdapter(installedMapList);
     listView.setAdapter((ListAdapter)installedAdapter);
+    mFragmentView = getActivity().getLayoutInflater().inflate(R.layout.offline_map_introduction, container, false);
     return mFragmentView;
   }
   
   class OfflineMapAdapter extends BaseAdapter{
     
-    private ArrayList<MKOLSearchRecord> mapInfoList;
+    private ArrayList<MKOLUpdateElement> mapInfoList;
     
-    public OfflineMapAdapter(ArrayList<MKOLSearchRecord> infoList){
+    public OfflineMapAdapter(ArrayList<MKOLUpdateElement> infoList){
       mapInfoList = infoList;
     }
 
@@ -98,8 +116,8 @@ public class OfflineMapFragment extends Fragment{
       } else {
         viewholder = (ViewHolder) convertView.getTag();
       }
-      viewholder.list_item_name.setText(((MKOLSearchRecord)mapInfoList.get(position)).cityName);
-      viewholder.list_item_total_size.setText(Integer.toString(((MKOLSearchRecord)mapInfoList.get(position)).size));
+      viewholder.list_item_name.setText(((MKOLUpdateElement)mapInfoList.get(position)).cityName);
+      viewholder.list_item_total_size.setText(Integer.toString(((MKOLUpdateElement)mapInfoList.get(position)).size));
       return convertView;
     }
     
